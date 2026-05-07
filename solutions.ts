9061e8057d55765dbcc645d785df453af76069a3
Syed-Anwarul-Haque-Piash <syed88783@gmail.com>
2026-05-07 20:48:49 +0600

    final

--- solutions.ts
+++ solutions.ts
@@ -2,8 +2,9 @@
 const filterEvenNumbers=(array: number[]): number[] => {
     return array.filter(arr => arr % 2 === 0);
 }
-//output
-//console.log(filterEvenNumbers([1, 2, 3, 4, 5, 6]))
+
+
+
 
 
 //Problem 2
@@ -11,8 +12,7 @@ const filterEvenNumbers=(array: number[]): number[] => {
 const reverseString=(str: string): string => {
     return str.split('').reverse().join('');
 }
-//output
-//console.log(reverseString("typescript"))
+
 
 
 //prolem 3
@@ -27,18 +27,16 @@ const checkType=(value: StringOrNumber): string => {
     }
 }
 
-//console.log(checkType("Hello")); // Output: String
-//console.log(checkType(42)); // Output: Number
+
 
 //problem 4
 
 const getProperty = <T, K extends keyof T>(obj: T, key: K): T[K] => {
     return obj[key];
 }
-const user = { id: 1, name: "John Doe", age: 21 };
-const userInfo = getProperty(user, "name");
 
-//console.log(userInfo); // Output: John Doe
+
+
 
 //problem 5
 
@@ -52,9 +50,7 @@ const toggleReadStatus=(book: Book):Book & {isRead: boolean} => {
     return { ...book, isRead:true };
 }
 
-const myBook = { title: "TypeScript Guide", author: "Jane Doe", publishedYear: 2024 };
-const updatedBook = toggleReadStatus(myBook);
-console.log(updatedBook); // Output: { title: "TypeScript Guide", author: "Jane Doe", publishedYear: 2024, isRead: true }
+
 
 
 //problem 6
@@ -78,14 +74,18 @@ class Student extends Person{
         return `Name: ${this.name}, Age: ${this.age}, Grade: ${this.grade}`;
     }
 }
-const student = new Student("Alice", 20, "A");
-//console.log(student.getDetails());
+
+
+
 
 //problem 7
 
 const getIntersection=(array1: number[], array2: number[]): number[] => {
     return array1.filter(value => array2.includes(value));
 }
-//console.log(getIntersection([1, 2, 3, 4, 5], [3, 4, 5, 6, 7])); 
+
+
+
+
 
 
